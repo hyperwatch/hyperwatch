916c0552686174674e743387bf7954fd8a034701
François Hodierne <francois@hodierne.net>
2020-02-21 10:15:09 +0100

    refactor: remove memory-logs

--- src/modules/logs.js
+++ src/modules/logs.js
@@ -1,8 +1,6 @@
 const app = require('../apps/websocket');
 const pipeline = require('../lib/pipeline');
-const memoryIndex = require('../plugins/memory-logs');
 const { logIsAugmented } = require('../lib/util');
-const { parseFilterQuery } = require('../lib/filter');
 
 // Expose raw logs
 
@@ -23,12 +21,3 @@ app.streamToWebsocket('/logs/augmented', augmentedStream, {
 // Expose augmented logs for dashboard
 
 app.streamToWebsocket('/logs', augmentedStream.filter(logIsAugmented));
-
-// Keep logs in memory and expose as API endpoint
-
-augmentedStream.map(memoryIndex.index);
-
-app.get('/logs', (req, res) => {
-  const { query } = req;
-  memoryIndex.searchLogs(parseFilterQuery(query)).then(logs => res.send(logs));
-});
