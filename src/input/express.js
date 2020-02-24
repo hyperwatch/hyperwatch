126f07e765090a83f6a79a5cd3ac815c6e3baba6
François Hodierne <francois@hodierne.net>
2020-02-24 23:45:52 +0100

    feat: iterate on express input

--- src/input/express.js
+++ src/input/express.js
@@ -1,19 +1,37 @@
+const onFinished = require('on-finished');
+
 const { createLog } = require('../lib/util');
 
-function create({ name = 'Express Middleware', app }) {
+function create({ name = 'Express Middleware', app } = {}) {
   return {
     name: name,
-    start: ({ success, reject, status }) => {
-      app.use((req, res, next) => {
-        try {
-          const log = createLog(req, res);
-          success(log);
-        } catch (err) {
-          reject(err);
-        }
-        next();
-      });
+    start: function({ success, reject, status }) {
+      this.success = success;
+      this.reject = reject;
+      this.status = status;
       status(null, `Listening.`);
+      if (app) {
+        app.use(this.middleware);
+      }
+    },
+    middleware: function(req, res, next) {
+      req.startAt = new Date();
+      onFinished('res', () => {
+        const { success, reject } = this;
+        req.endAt = new Date();
+        try {
+          const executionTime = req.endAt - req.startAt;
+          const log = createLog(req, res).set('executionTime', executionTime);
+          if (success) {
+            success(log);
+          }
+        } catch (err) {
+          if (reject) {
+            reject(err);
+          }
+        }
+      });
+      next();
     },
   };
 }
