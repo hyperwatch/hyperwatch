c9b52b1dd1bfaec06ebec4d873ad83ceed3cd862
François Hodierne <francois@hodierne.net>
2020-03-02 23:04:14 +0100

    feat: hostname plugin

--- src/plugins/hostname.js
+++ src/plugins/hostname.js
@@ -1,23 +1,22 @@
 const dns = require('dns').promises;
-// const { Map } = require('immutable');
 
 async function lookup(log) {
   const ip = log.getIn(['address', 'value']);
-  const reverses = await dns.reverse(ip);
-  if (reverses) {
-    const reverse = reverses[0];
-    log = log.setIn(['hostname', 'value'], reverse);
-    try {
+  try {
+    const reverses = await dns.reverse(ip);
+    if (reverses) {
+      const reverse = reverses[0];
+      log = log.setIn(['hostname', 'value'], reverse);
+      log = log.setIn(['address', 'hostname'], reverse);
       const reverseIps = await dns.resolve(reverse);
       const reverseIp = reverseIps[0];
-      console.log(reverseIp, ip);
       if (reverseIp === ip) {
         log = log.setIn(['hostname', 'verified'], true);
       }
-    } catch (error) {
-      console.log(error);
-      // Ignore error
     }
+  } catch (error) {
+    // Ignore error
+    // console.log(error);
   }
   return log;
 }
