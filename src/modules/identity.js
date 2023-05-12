c184ef80f9d1dcc2188698a73675b2d570483377
François Hodierne <francois@hodierne.net>
2023-05-12 13:57:06 +0200

    fix: remove previous Amazonbot entry

--- src/modules/identity.js
+++ src/modules/identity.js
@@ -266,10 +266,6 @@ function augment(log) {
         hostname.endsWith('.eu-central-1.compute.amazonaws.com')
         ? log.set('identity', 'Wise')
         : log;
-    case 'Amazonbot':
-      return hostname && hostname.endsWith('.compute-1.amazonaws.com')
-        ? log.set('identity', 'Amazonbot')
-        : log;
 
     // GCE
     case 'VelenPublicWebCrawler':
