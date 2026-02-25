985d80a9b8c4c938246c7d00dfcdca563633274a
François Hodierne <francois@hodierne.net>
2026-02-25 13:15:40 +0100

    Add RootCrawl and Headline (ev-crawler) identities (#498)

--- src/modules/identity.js
+++ src/modules/identity.js
@@ -180,6 +180,10 @@ function augment(log) {
       return hostname && hostname.endsWith('.neevabot.com')
         ? log.set('identity', 'Neevabot')
         : log;
+    case 'RootCrawl-Crawler':
+      return hostname && hostname.endsWith('.rootcrawl.org')
+        ? log.set('identity', 'RootCrawl')
+        : log;
     case 'DataForSeoBot':
       return hostname && hostname.endsWith('.dataforseo.com')
         ? log.set('identity', 'DataForSeo')
@@ -328,6 +332,10 @@ function augment(log) {
     case 'MegaIndex.ru':
       return hostname && hostname.endsWith('.clients.your-server.de')
         ? log.set('identity', 'MegaIndex.ru')
+        : log;
+    case 'ev-crawler':
+      return hostname && hostname.endsWith('.headline.com')
+        ? log.set('identity', 'Headline')
         : log;
   }
 
